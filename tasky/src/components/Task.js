15f7e244b0547a358210ad415857e2226ed4ce51
Jack McGrath <jackmcgrath2k@gmail.com>
2022-02-01 09:58:05 +0000

    Week 3 Lab A done

--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -3,10 +3,13 @@ import React from 'react';
 const Task = (props) => {
 
     return (
-        <div className="card">
+        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p>{props.description}</p>
+            <button onClick={props.markDone} className='doneButton'>Done</button>
+            <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
+            
         </div>
     )
     return (
@@ -14,6 +17,7 @@ const Task = (props) => {
             <p>{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p>{props.description}</p>
+            
         </div>
     )
 
